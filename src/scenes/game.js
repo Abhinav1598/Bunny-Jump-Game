1c04f2ec757eb37414c440082f69d7743a2da876
Abhinav Singh <abhinavsingh16160@gmail.com>
2020-09-01 20:24:50 +0530

    created carrots and bunny can collect now

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1,5 +1,9 @@
 import Phaser from '../lib/phaser.js'
 
+//import the carrot class here.
+import Carrot from '../game/carrot.js'
+
+
 export default class Game extends Phaser.Scene 
 {   
 
@@ -11,6 +15,9 @@ export default class Game extends Phaser.Scene
 
     /** @type {Phaser.Types.Input.Keyboard.CursorKeys} */
     cursors
+
+    /** @type {Phaser.Physics.Arcade.Group} */
+    carrots
 
     /**
      *  @param {Phaser.GameObjects.sprite} sprite
@@ -29,6 +36,26 @@ export default class Game extends Phaser.Scene
              sprite.x = -halfWidth
             
         }
+    }
+    
+    /**
+     * @param {Phaser.GameObjects.sprite} sprite
+     */
+
+    addCarrotsAbove(sprite){
+        const y = sprite.y - sprite.displayHeight;
+        /** @type {Phaser.Physics.Arcade.Sprite} */
+        const carrot = this.carrots.get(sprite.x,y,'carrot');
+        carrot.setActive(true);
+        carrot.setVisible(true);
+        this.add.existing(carrot);
+
+        //update the physics body size
+       carrot.body.setSize(carrot.width,carrot.height);
+
+       this.physics.world.enable(carrot);
+
+        return  carrot;
     } 
 
     constructor(){
@@ -40,6 +67,9 @@ export default class Game extends Phaser.Scene
         this.load.image('platform','assets/ground_grass.png');
         this.load.image('bunny-stand','assets/bunny1_stand.png');
         this.load.image('bg','assets/bg_layer2.png');
+        
+        //adding carrot png
+        this.load.image('carrot', 'assets/carrot.png');
 
         this.cursors = this.input.keyboard.createCursorKeys();
         
@@ -50,6 +80,10 @@ export default class Game extends Phaser.Scene
         this.add.image(240,320,'background').setScrollFactor(1,0);
         this.add.image(240,320,'bg');
         this.cameras.main.setDeadzone(this.scale.width*1.5);
+
+        //create carrot instance here from Carrot class
+       /*  const carrot = new Carrot(this, 240, 320, 'carrot');
+        this.add.existing(carrot);  */
 
         //good for creating a single image 
         //this.physics.add.staticImage(240,320,'platform').setScale(0.5); 
@@ -83,7 +117,30 @@ export default class Game extends Phaser.Scene
         this.cameras.main.startFollow(this.player);
 
 
+        // adding carrots
+
+        this.carrots =  this.physics.add.group({
+            classType:Carrot
+        })
         
+
+
+        //adding collider between platform and carrots 
+        this.physics.add.collider(this.platforms,this.carrots);
+
+        //collecting carrots
+
+        this.physics.add.overlap(
+            this.player,
+            this.carrots,
+            this.handleCollectCarrot, // called an overlap
+            undefined,
+            this            
+        )
+
+
+
+          
 
 
     }
@@ -102,10 +159,31 @@ export default class Game extends Phaser.Scene
 
             if(platform.y >= scrollY + 700){
 
-                platform.y = scrollY - 100;
+                platform.y = scrollY - Phaser.Math.Between(70, 100);
+                platform.x = scrollX + Phaser.Math.Between(0, 450); 
                 platform.body.updateFromGameObject();
+
+                //add carrot above platform
+                this.addCarrotsAbove(platform);
             }
+
+            
         })
+
+       /*  // remove the oprhaned carrots 
+
+        this.carrots.children.iterate(child => {
+            const carrot = child;
+            const scrollY = this.cameras.main.scrollY;
+            if(carrot.y > scrollY + 700){
+
+                
+                this.carrots.kill(carrot);
+                this.physics.world.disableBody(carrot.body);
+                //remove carrots from the physical world
+                
+            }
+        }) */
 
         // left and right input logic
         if(this.cursors.left.isDown && !touchingDown){
@@ -122,6 +200,22 @@ export default class Game extends Phaser.Scene
     }
 
 
+
+    // handle colledtor method for collecting the carrots 
+
+   /**
+   * @param {Phaser.Physics.Arcade.Sprite} player
+   * @param {Carrot} carrot
+   */
+
+    handleCollectCarrot(player,carrot){
+
+        this.carrots.killAndHide(carrot);
+
+        // disable from physics world
+        this.physics.world.disableBody(carrot.body);
+        
+    }
 
 
 
